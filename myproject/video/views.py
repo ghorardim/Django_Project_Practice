75d3a172374f85a434d670be7b1f2cb44d2fcf40
ghorardim <aamjabir@gmail>
2018-12-24 17:25:56 +0600

    After use django libray login and register form

--- myproject/video/views.py
+++ myproject/video/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http.response import HttpResponse
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
 from  .models import Video
+from  .forms import LoginForm
 
 # Create your views here.
 
@@ -11,16 +15,32 @@ def home(request):
 
 def login(request):
 
-    if request.method =="POST":
-        username = request.POST.get('username',None)
-        password = request.POST.get('password',None)
-
-        if username is not None and password is not None and username != '' and password != '':
-            user = authenticate(username = username, password = password)
-            if user:
-                return redirect("/")
-            else:
-                return HttpResponse("Login Field")
+    if request.method =='POST':
+        form = LoginForm(request.POST)
+        user = form.authenticate()
+        if user:
+            return redirect("/home")
         else:
-            return  HttpResponse("Empty Value Detectated")
-    return render(request,'login.html')
+            return HttpResponse('Form is not valid')
+
+    else:
+        form = LoginForm()
+        return render(request,'login.html',{'form': form})
+
+def register(request):
+    print("call hocce")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            print("user form paice")
+            if user:
+                return  HttpResponse("User ID: " + str(user.id) + " created")
+                print("user er account hoice")
+            else:
+                return  HttpResponse("ERROR")
+                print("user error")
+    else:
+        form = UserCreationForm()
+        print("user kicchu hoy nai")
+    return render(request,'register.html',{'form':form})
